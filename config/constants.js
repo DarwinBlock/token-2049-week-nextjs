139684df3d238863c85d03287ca8c265e0a3ab1b
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2023-11-23 14:50:50 +0530

    refactor updates

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,2 @@
 export const current_date = "2023-09-11";
 export const website_url = 'https://week.token2049.com/';
-export const username = "scorpio";
-export const userpass = "T1me@z0ne!1";
-export const dbname = "weekcryptdb";
