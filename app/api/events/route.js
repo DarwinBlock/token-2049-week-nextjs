b875bba97fe2680ba46be099e9e6d00726d40499
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2023-11-24 18:29:12 +0530

    form integration testing

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -2,16 +2,27 @@ import getEvents from "@/backend/controllers/eventController";
 import { NextResponse } from "next/server";
 
 export async function GET(){
-    const data = await getEvents();
-    return NextResponse.json({
-        events: data,
-    })
+    try{
+        const data = await getEvents();
+        return NextResponse.json({
+            events: data,
+        })
+    }
+    catch(e){
+        return NextResponse.json({ message: e }, { status: 500 })
+    }
 }
 
 export async function POST(request){
-    console.log(request);
-    console.log(await request.json());
+    const formDataJSON = await request.json();
+    console.log("-----------------FORM DATA-----------------------",formDataJSON);
+    const formFields = formDataJSON.data.fields;
+    console.log("-----------------FORM FIELDS-----------------------",formFields);
+    
+    formFields?.forEach((item, idx) => {
+        console.log(`----------------ITEM ${idx}--------------------`, item);
+    })
     return NextResponse.json({
-        request
+        data: "recved"
     })
 }
