31e4375e6429b879eb545d7cba5d3f0281f7fc1b
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2023-12-17 23:16:14 +0530

    axios error

--- components/admin/Modals/DeleteToolConfirmModal.js
+++ components/admin/Modals/DeleteToolConfirmModal.js
@@ -1,5 +1,6 @@
 import Modal from "@/components/ui/Modal";
 import Image from "next/image";
+import axios from "axios";
 
 const DeleteToolConfirmModal = ({ isOpen, setOpen, toolToDelete }) => {
 	const deleteToolHandler = async () => {
