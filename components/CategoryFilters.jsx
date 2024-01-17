7f618cdc5257ebda8e75bb2381d93ac43583a4c0
pushpit07 <pushpit07@gmail.com>
2024-01-18 00:07:27 +0100

    Added Sports category
    Signed-off-by: pushpit07 <pushpit07@gmail.com>

--- components/CategoryFilters.jsx
+++ components/CategoryFilters.jsx
@@ -1,52 +1,33 @@
 import CategoryFilterButton from "./CategoryFilterButton";
 
-const CategoryFilter = ({selectedCategory, setSelectedCategory}) => {
-    return (  
-        <>
-            <CategoryFilterButton
-                buttonLabel={'All'}
-                isActive={selectedCategory === 'all'}
-                onClick={setSelectedCategory}
-                category='all'
-            />
-            <CategoryFilterButton
-                buttonLabel={'Workshop / Hackathon'}
-                isActive={selectedCategory === 'workshop'}
-                onClick={setSelectedCategory}
-                category='workshop'
-            />
-            <CategoryFilterButton
-                buttonLabel={'Party / Dinner'}
-                isActive={selectedCategory === 'party'}
-                onClick={setSelectedCategory}
-                category='party'
-            />
-            <CategoryFilterButton
-                buttonLabel={'Conference / Summit'}
-                isActive={selectedCategory === 'conference'}
-                onClick={setSelectedCategory}
-                category='conference'
-            />
-            {/* <CategoryFilterButton
+const CategoryFilter = ({ selectedCategory, setSelectedCategory }) => {
+	return (
+		<>
+			<CategoryFilterButton buttonLabel={"All"} isActive={selectedCategory === "all"} onClick={setSelectedCategory} category="all" />
+			<CategoryFilterButton
+				buttonLabel={"Workshop / Hackathon"}
+				isActive={selectedCategory === "workshop"}
+				onClick={setSelectedCategory}
+				category="workshop"
+			/>
+			<CategoryFilterButton buttonLabel={"Party / Dinner"} isActive={selectedCategory === "party"} onClick={setSelectedCategory} category="party" />
+			<CategoryFilterButton
+				buttonLabel={"Conference / Summit"}
+				isActive={selectedCategory === "conference"}
+				onClick={setSelectedCategory}
+				category="conference"
+			/>
+			{/* <CategoryFilterButton
                 buttonLabel={'Gaming'}
                 isActive={selectedCategory === 'gaming'}
                 onClick={setSelectedCategory}
                 category='gaming'
             /> */}
-            <CategoryFilterButton
-                buttonLabel={'Networking'}
-                isActive={selectedCategory === 'networking'}
-                onClick={setSelectedCategory}
-                category='networking'
-            />
-            <CategoryFilterButton
-                buttonLabel={'Other'}
-                isActive={selectedCategory === 'other'}
-                onClick={setSelectedCategory}
-                category='other'
-            />
-        </>
-    );
-}
- 
+			<CategoryFilterButton buttonLabel={"Networking"} isActive={selectedCategory === "networking"} onClick={setSelectedCategory} category="networking" />
+			<CategoryFilterButton buttonLabel={"Sports"} isActive={selectedCategory === "sports"} onClick={setSelectedCategory} category="sports" />
+			<CategoryFilterButton buttonLabel={"Other"} isActive={selectedCategory === "other"} onClick={setSelectedCategory} category="other" />
+		</>
+	);
+};
+
 export default CategoryFilter;
